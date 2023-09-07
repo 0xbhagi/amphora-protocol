2de4bbb01eb5ceed85aaebe2ba4c6d1c90e04be0
Bhagi.eth <mogithehurter@gmail.com>
2023-09-07 13:15:15 +0530

    Initial commit

---
+++ src/amphora-protocol-token.ts
@@ -0,0 +1,92 @@
+import {
+  Approval as ApprovalEvent,
+  DelegateChanged as DelegateChangedEvent,
+  DelegateVotesChanged as DelegateVotesChangedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/AmphoraProtocolToken/AmphoraProtocolToken"
+import {
+  Approval,
+  DelegateChanged,
+  DelegateVotesChanged,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateChanged(event: DelegateChangedEvent): void {
+  let entity = new DelegateChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegator = event.params.delegator
+  entity.fromDelegate = event.params.fromDelegate
+  entity.toDelegate = event.params.toDelegate
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateVotesChanged(
+  event: DelegateVotesChangedEvent
+): void {
+  let entity = new DelegateVotesChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegate = event.params.delegate
+  entity.previousBalance = event.params.previousBalance
+  entity.newBalance = event.params.newBalance
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
